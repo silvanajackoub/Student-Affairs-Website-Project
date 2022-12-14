c162a0bdb1c1611a77b00b47e6cf0101e9873d90
JohnnGamil <johnngamil@gmail.com>
2022-12-14 04:07:07 +0200

    Handled all functions of the website

--- project/settings.py
+++ project/settings.py
@@ -129,6 +129,8 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'affairss08@gmail.com'
 EMAIL_HOST_PASSWORD = 'kopecktgrdriwfzf'
